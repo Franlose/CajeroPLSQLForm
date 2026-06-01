ed6cefa53cc6f5c30d11ce68f29422890dbb584e
flopezse87 <franciscols@ext.inditex.com>
2026-06-01 13:19:40 +0200

    T1.5: Programada lógica de retirada por billete con control de stock y saldos

--- database/packages/PKG_CAJERO_PKB.sql
+++ database/packages/PKG_CAJERO_PKB.sql
@@ -16,7 +16,7 @@ CREATE OR REPLACE PACKAGE BODY hr.pkg_cajero IS
          WHERE id_usuario = p_id_usuario AND pin = p_pin;
            
         p_valido := TRUE;
-    EXCEPTION
+    EXCEPTION -- T1.3
         -- Si las credenciales no coinciden, evitamos que la app se rompa
         WHEN NO_DATA_FOUND THEN p_valido := FALSE;
             p_es_admin := 'N';
@@ -33,8 +33,44 @@ CREATE OR REPLACE PACKAGE BODY hr.pkg_cajero IS
         v_valor_billete NUMBER;
         v_stock_cajero  NUMBER;
     BEGIN
+		-- Obtenemos el valor económico del billete seleccionado (ej: 20, 50)
+        SELECT id_billete INTO v_valor_billete FROM hr.tipos_billetes WHERE id_billete = p_id_billete;
 
-        RETURN False; -- Pendiente del punto 1.5
+        -- Bloqueamos la cuenta y validamos si el cliente tiene saldo suficiente
+        SELECT saldo INTO v_saldo_actual FROM hr.usuarios_cuentas WHERE id_usuario = p_id_usuario
+           FOR UPDATE;
+
+        IF v_saldo_actual < v_valor_billete THEN RETURN FALSE; -- Saldo insuficiente en cuenta
+        END IF;
+
+        -- Bloqueamos el stock y comprobamos si queda al menos 1 billete en el cajero
+        SELECT cantidad INTO v_stock_cajero FROM hr.cajero_stock WHERE id_billete = p_id_billete
+           FOR UPDATE;
+
+        IF v_stock_cajero < 1 THEN RETURN FALSE; -- El cajero no tiene físicamente este tipo de billete
+        END IF;
+
+        -- Si todo es correcto, aplicamos los cambios transaccionales
+        -- Restamos 1 unidad del stock físico del cajero
+        UPDATE hr.cajero_stock SET cantidad = cantidad - 1 WHERE id_billete = p_id_billete;
+
+        -- Restamos el importe económico del saldo del usuario
+        UPDATE hr.usuarios_cuentas SET saldo = saldo - v_valor_billete WHERE id_usuario = p_id_usuario;
+
+        -- Registramos la auditoría de la retirada en la tabla MOVIMIENTOS
+        INSERT INTO hr.movimientos (
+            id_mov, id_usuario, fecha, tipo_operacion, 
+            id_billete, cantidad_billetes, total_importe
+        ) VALUES (
+            hr.seq_movimientos.NEXTVAL, p_id_usuario, SYSDATE, 'R', 
+            p_id_billete, 1, v_valor_billete
+        );
+
+        RETURN TRUE; -- Operación autorizada con éxito
+    EXCEPTION -- T1.3
+        WHEN OTHERS THEN
+            ROLLBACK; -- Deshacemos cualquier cambio en caso de error inesperado
+            RAISE_APPLICATION_ERROR(-20001, 'Error crítico en el proceso de retirada.');
     END retirar_dinero;
 
     --------------------------------------------------------------------
